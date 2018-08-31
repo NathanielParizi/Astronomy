444c34e88949a4f9e569a074da89dd973932ceef
justi <justi@DESKTOP-O7K8SPS.Home>
2018-08-31 14:47:01 -0400

    test

---
+++ astro.m
@@ -0,0 +1,27 @@
+
+currentFastestCraft_KM = 50000;
+
+%Suppose you wanted to reach Alpha Centauri
+%(4.4 ly from the Solar system) in 170 years.
+%How fast would you have to go, in km/hr?
+
+alphaCentauri = 28000000;
+
+% p = orbital period
+% a = semimajor axis
+% e = eccentricity
+
+ a = 3;
+ b= 4;
+
+ c = sqrt(a^2+b^2)
+%  b = sqrt(c^2-a^2)
+%  a =sqrt(c^2-b^2)
+ 
+ 
+
+
+% exp(1)
+% 
+% exp(2)
+
